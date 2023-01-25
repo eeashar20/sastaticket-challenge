186367f6c1566f758e6beb0a2ace84be5b623c0d
eeashar20 <ashar_haroon2006@yahoo.com>
2023-01-25 17:33:42 +0500

    Add Order CRUD API.

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from store.views import CustomerViewSet
+from store.views import CustomerViewSet, OrderViewSet
 
 router = DefaultRouter()
 router.register('customers', CustomerViewSet, basename='customers')
+router.register('orders', OrderViewSet, basename='orders')
 
 urlpatterns = router.urls
--- store/views.py
+++ store/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
-from store.models import Customer
-from store.serializers import CustomerSerializer
+from store.models import Customer, Order
+from store.serializers import CustomerSerializer, OrderSerializer
 
 
 # Create your views here.
@@ -13,3 +13,11 @@ class CustomerViewSet(ModelViewSet):
     def get_queryset(self):
         authenticated_user = self.request.user.id
         return Customer.objects.filter(user_id=authenticated_user)
+
+
+class OrderViewSet(ModelViewSet):
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        authenticated_user = self.request.user.id
+        return Order.objects.filter(customer__user_id=authenticated_user)
