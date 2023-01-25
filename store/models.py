26287a24c82aa7f458c15753a166e68934934c31
eeashar20 <ashar_haroon2006@yahoo.com>
2023-01-25 16:38:37 +0500

    Add models in store app and run migrations.

--- store/models.py
+++ store/models.py
@@ -1,3 +1,35 @@
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='customer')
+    birth_date = models.DateField(null=True, blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT, related_name='orders')
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='orderitems')
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
