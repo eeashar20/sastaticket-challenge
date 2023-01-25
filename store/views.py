32030aad565f1e50e58882bb3a75303d3cca8aca
eeashar20 <ashar_haroon2006@yahoo.com>
2023-01-25 17:19:03 +0500

    Add CRUD API for customers.

--- store/views.py
+++ store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from store.models import Customer
+from store.serializers import CustomerSerializer
+
 
 # Create your views here.
+
+class CustomerViewSet(ModelViewSet):
+    serializer_class = CustomerSerializer
+
+    def get_queryset(self):
+        authenticated_user = self.request.user.id
+        return Customer.objects.filter(user_id=authenticated_user)
